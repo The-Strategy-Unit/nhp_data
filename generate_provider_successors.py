346b90aa3e2e11ea8bfdddba55e52b898ea9efbc
Tom Jemmett <thomas.jemmett@nhs.net>
2024-10-16 10:14:01 +0100

    add some additional filters to successors
    * remove the edge case of RW6 succeeded by two orgs
    * only include NHS Trusts in the successors list

--- generate_provider_successors.py
+++ generate_provider_successors.py
@@ -12,6 +12,7 @@
 
 from typing import Iterator
 
+import pyspark.sql.functions as F
 import requests
 from databricks.connect import DatabricksSession
 from delta.tables import DeltaTable
@@ -37,19 +38,34 @@ def get_predecessors(org_code: str) -> Iterator[str]:
     :yield: A list containing ODS organisation codes of the organisation and it's predecessors
     :rtype: Iterator[str]
     """
-    yield org_code
-    r = (
-        requests.get(
-            f"https://uat.directory.spineservices.nhs.uk/ORD/2-0-0/organisations/{org_code}",
-            timeout=10,
-        )
-        .json()["Organisation"]
-        .get("Succs", {"Succ": []})
-    )
+    r = requests.get(
+        f"https://uat.directory.spineservices.nhs.uk/ORD/2-0-0/organisations/{org_code}",
+        timeout=10,
+    ).json()["Organisation"]
 
-    for s in r["Succ"]:
-        if s["Type"] == "Predecessor":
-            yield from get_predecessors(s["Target"]["OrgId"]["extension"])
+    # only include NHS Trusts
+    if r["Roles"]["Role"][0]["id"] != "RO197":
+        return
+
+    # return the current organisation code
+    yield org_code
+
+    # iterate over the successor records
+    # handle the case of not having any successor records
+    if "Succs" not in r:
+        return
+
+    # extract just the predecessor records
+    predecessor_records = filter(
+        lambda x: x["Type"] == "Predecessor", r["Succs"]["Succ"]
+    )
+    # recursively call get_predecessors on the predecessors
+    for i in predecessor_records:
+        predecessor = i["Target"]["OrgId"]["extension"]
+        # handle the edge case of Peninne / MFT / NCA
+        if org_code == "R0A" and predecessor == "RW6":
+            continue
+        yield from get_predecessors(predecessor)
 
 
 # COMMAND ----------
@@ -59,7 +75,40 @@ provider_successors = spark.read.json(
         [{"old_code": q, "new_code": p} for p in providers for q in get_predecessors(p)]
     )
 )
+
+# show the results
 provider_successors.display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## handle edge cases
+
+# COMMAND ----------
+
+# Remove RAV, which closed in 1993, but was succeeded by RJ1 and RJ2
+provider_successors = provider_successors.filter(F.col("old_code") != "RAV")
+
+# COMMAND ----------
+
+# there is at least one case of A<-B, and A<-C<-B, resulting in two records for A<-B. Remove these
+provider_successors = provider_successors.distinct()
+
+# COMMAND ----------
+
+# Check there are no cases remaining of one organisation being succeeded twice
+assert (
+    provider_successors.groupBy("old_code").count().filter(F.col("count") > 1).count()
+    == 0
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## save table
+# MAGIC
+# MAGIC use an upsert to update the table
+
 
 # COMMAND ----------
 
