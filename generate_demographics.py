36b1b6049fab59dbaba8f6b659eeecc0bb53013a
Tom Jemmett <thomas.jemmett@nhs.net>
2025-02-14 16:42:20 +0000

    adds npp variants

--- generate_demographics.py
+++ generate_demographics.py
@@ -1,7 +1,15 @@
 # Databricks notebook source
+# MAGIC %pip install xlrd
+
+# COMMAND ----------
+
+import re
+
+import pandas as pd
 from databricks.connect import DatabricksSession
 from pyspark.sql import Window
 from pyspark.sql import functions as F
+from tqdm.auto import tqdm
 
 spark = DatabricksSession.builder.getOrCreate()
 spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
@@ -40,7 +48,7 @@ def create_demographic_parquet(projection_name: str) -> None:
     for sex_int, sex_string in [(1, "males"), (2, "females")]:
         (
             spark.read.csv(
-                f"{path}/{file_name}/2018 SNPP Population {sex_string}.csv",
+                f"{path}/snpp_2018b_{file_name}/2018 SNPP Population {sex_string}.csv",
                 header=True,
             )
             .filter(F.col("AGE_GROUP") != "All ages")
@@ -131,6 +139,90 @@ for i in projections:
 # COMMAND ----------
 
 # MAGIC %md
+# MAGIC ## NPP
+
+# COMMAND ----------
+
+principal_proj = spark.read.parquet(f"{path}/demographic_data").filter(
+    F.col("projection") == "principal_proj"
+)
+
+# COMMAND ----------
+
+principal_proj = (
+    spark.read.parquet(f"{path}/demographic_data")
+    .filter(F.col("projection") == "principal_proj")
+    .persist()
+)
+
+
+def create_npp_variant(file: str, projection_name: str) -> None:
+    """Create NPP Parquet Files
+
+    Create the national projections at subnational level
+
+    :param file: The name of the file to load
+    :type file: str
+    :param projection_name: The name of the projection
+    :type projection_name: str
+    """
+    df = pd.read_excel(
+        f"{path}/npp_2018b/en_{file}_opendata2018.xls", sheet_name="Population"
+    )
+    df = df.rename(columns={"Age": "age", "Sex": "sex"})
+    df["age"] = [
+        min(90, int(re.sub("\s*(\\d+).*", "\\1", i))) for i in df["age"].to_list()
+    ]
+    df = df.groupby(["sex", "age"], as_index=False).sum()
+    df = df.melt(id_vars=["age", "sex"], var_name="year", value_name="pop")
+    df["year"] = df["year"].astype("int")
+    df.set_index(["year", "sex", "age"], inplace=True)
+
+    df = df.loc[(slice(2018, 2043), slice(None), slice(None))].reset_index()
+
+    df = spark.createDataFrame(df)
+
+    w = Window.partitionBy("year", "sex", "age")
+    (
+        principal_proj.join(df, ["year", "sex", "age"])
+        .withColumn("value", F.col("value") * F.col("pop") / F.sum("value").over(w))
+        .drop("pop")
+        .orderBy("age")
+        .repartition(1)
+        .write.mode("overwrite")
+        .partitionBy("sex", "area_code")
+        .parquet(f"{path}/demographic_data/projection={projection_name}")
+    )
+
+
+# COMMAND ----------
+
+variants = [
+    ("hpp", "high_fertility"),
+    ("lpp", "low_fertility"),
+    ("php", "high_life_expectancy"),
+    ("plp", "low_life_expectancy"),
+    ("pph", "high_intl_migration"),
+    ("ppl", "low_intl_migration"),
+    ("hhh", "high_population"),
+    ("lll", "low_population"),
+    ("lhl", "old_age_structure"),
+    ("hlh", "young_age_structure"),
+    ("ppz", "zero_net_migration"),
+    ("pnp", "no_mortality_improvement"),
+    ("cnp", "const_fert_no_mort_imp"),
+    ("cpp", "const_fertility"),
+    ("rpp", "replacement_fertility"),
+    ("ppr", "half_eu_migration"),
+    ("ppq", "zero_eu_migration"),
+]
+
+for v in tqdm(variants):
+    create_npp_variant(*v)
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC
 # MAGIC # Create demographics for model
 # MAGIC
@@ -142,15 +234,17 @@ for i in projections:
 # MAGIC %md
 # MAGIC
 # MAGIC ## Create national demographics file
-
-(
-    spark.read.parquet(f"{path}/demographic_data")
-    .filter(F.col("area_code").rlike("^E0[6-9]"))
-    .groupBy("projection", "sex", "age")
-    .pivot("year")
-    .agg(F.sum("value").alias("value"))
-    .orderBy("projection", "sex", "age")
-)
+# MAGIC
+# MAGIC ```
+# MAGIC (
+# MAGIC     spark.read.parquet(f"{path}/demographic_data")
+# MAGIC     .filter(F.col("area_code").rlike("^E0[6-9]"))
+# MAGIC     .groupBy("projection", "sex", "age")
+# MAGIC     .pivot("year")
+# MAGIC     .agg(F.sum("value").alias("value"))
+# MAGIC     .orderBy("projection", "sex", "age")
+# MAGIC )
+# MAGIC ```
 
 # COMMAND ----------
 
@@ -187,5 +281,3 @@ df = (
 # COMMAND ----------
 
 df
-
-# COMMAND ----------
