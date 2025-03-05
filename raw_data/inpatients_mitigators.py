f814a8ef6edfdcdf4dd0c8b281cf0443a1f42207
Tom Jemmett <thomas.jemmett@nhs.net>
2025-03-05 14:42:49 +0000

    converts notebooks to scripts

--- raw_data/inpatients_mitigators.py
+++ raw_data/inpatients_mitigators.py
@@ -1,65 +1,56 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC
-# MAGIC # Generate inpatient mitigators
-# MAGIC
-
-# COMMAND ----------
-
-# MAGIC %pip install tqdm
-
-# COMMAND ----------
+"""Generate inpatient mitigators"""
 
 import importlib
 import os
 
 from databricks.connect import DatabricksSession
+from pyspark.context import SparkContext
 from tqdm.auto import tqdm
 
-import mitigators
+import raw_data.mitigators as mitigators
 
-spark = DatabricksSession.builder.getOrCreate()
 
-# COMMAND ----------
+def generate_inpatients_mitigators(spark: SparkContext) -> None:
+    """Generrate Inpatients Mitigators"""
+    path = ["mitigators", "ip"]
 
-path = ["mitigators", "ip"]
+    for i in ["activity_avoidance", "efficiency"]:
+        for j in sorted(os.listdir("/".join(path + [i]))):
+            if j == "__init__.py":
+                continue
+            module = ".".join(path + [i, j])[:-3]
+            try:
+                importlib.import_module(module)
+            except:  # pylint: disable=bare-except
+                print(f"Error: {module}")
 
-for i in ["activity_avoidance", "efficiency"]:
-    for j in sorted(os.listdir("/".join(path + [i]))):
-        if j == "__init__.py":
-            continue
-        module = ".".join(path + [i, j])[:-3]
+    all_mitigators = [
+        v2
+        for v1 in mitigators.__registered_mitigators.values()  # pylint: disable=protected-access
+        for v2 in v1.values()
+    ]
+
+    errors = {}
+
+    for m in (pbar := tqdm(all_mitigators)):
+        pbar.set_description(f"Processing {m}:")
+
         try:
-            importlib.import_module(module)
-        except:  # pylint: disable=bare-except
-            print(f"Error: {module}")
+            m.save()
+        except Exception as e:  # pylint: disable=broad-exception-caught
+            errors[str(m)] = e
 
-# COMMAND ----------
+    print(errors)
 
-all_mitigators = [
-    v2
-    for v1 in mitigators.__registered_mitigators.values()  # pylint: disable=protected-access
-    for v2 in v1.values()
-]
+    (
+        spark.read.table("nhp.raw_data.apc_mitigators")
+        .groupBy("type", "strategy")
+        .count()
+        .orderBy("type", "strategy")
+        .display()
+    )
 
-errors = {}
 
-for m in (pbar := tqdm(all_mitigators)):
-    pbar.set_description(f"Processing {m}:")
-
-    try:
-        m.save()
-    except Exception as e:
-        errors[str(m)] = e
-
-errors
-
-# COMMAND ----------
-
-(
-    spark.read.table("nhp.raw_data.apc_mitigators")
-    .groupBy("type", "strategy")
-    .count()
-    .orderBy("type", "strategy")
-    .display()
-)
+if __name__ == "__main__":
+    spark = DatabricksSession.builder.getOrCreate()
+    generate_inpatients_mitigators(spark)
