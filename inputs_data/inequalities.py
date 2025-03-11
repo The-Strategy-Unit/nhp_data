38835c1ffc15266f86e56086c464ed98e912adcd
yiwen-h <yiwen-h>
2025-03-11 10:35:01 +0000

    Adds script for calculating inequalities to inputs_data
    Closes #44

--- inputs_data/inequalities.py
+++ inputs_data/inequalities.py
@@ -30,10 +30,10 @@ def load_inequalities_data(fyears):
     """
 
     imd_df = (
-        spark.read.table("su_data.nhp.population_by_imd_decile")
-        .withColumn("imd19_quintile", F.floor((F.col("imd19") - 1) / 2) + 1)
+        spark.read.table("nhp.reference.population_by_imd_decile")
+        .withColumn("imd_quintile", F.floor((F.col("imd19") - 1) / 2) + 1)
         .drop("imd19")
-        .groupby("provider", "imd19_quintile")
+        .groupby("provider", "imd_quintile")
         .agg(F.sum("pop").alias("pop"))
         .withColumn(
             "population_share",
@@ -46,31 +46,38 @@ def load_inequalities_data(fyears):
     )
 
     apc = (
-        spark.read.table("su_data.nhp.apc")
+        spark.read.table("nhp.default.apc")
         .filter(F.col("fyear").isin(fyears))
         .withColumn("sushrg_trimmed", F.expr("substring(sushrg, 1, 4)"))
         .filter(F.col("admimeth").startswith("1"))
-        .select("provider", "imd19_quintile", "sushrg_trimmed", "fyear")
+        .select("provider", "imd_quintile", "sushrg_trimmed", "fyear")
+        .groupby("provider", "imd_quintile", "sushrg_trimmed", "fyear")
+        .agg(F.count("*").alias("count"))
     )
 
     opa = (
-        spark.read.table("su_data.nhp.opa_ungrouped")
+        spark.read.table("nhp.default.opa")
         .filter(F.col("fyear").isin(fyears))
         .filter(F.col("has_procedures") == True)
-        .select("provider", "imd19_quintile", "sushrg_trimmed", "fyear")
+        .groupby("provider", "imd_quintile", "sushrg_trimmed", "fyear")
+        .agg(F.sum("attendances").alias("count"))
     )
 
     data = opa.unionByName(apc)
 
     data_hrg_count = (
-        data.groupby(["provider", "sushrg_trimmed", "imd19_quintile", "fyear"])
-        .agg(F.count("*").alias("count"))
-        .join(imd_df, on=["provider", "imd19_quintile"])
+        data.groupby(["provider", "imd_quintile", "sushrg_trimmed", "fyear"])
+        .agg(F.sum("count").alias("count"))
+        .join(imd_df, on=["provider", "imd_quintile"])
         .withColumn("activity_rate", F.col("count") / (F.col("pop")))
     )
+
     return data_hrg_count
 
-def calculate_inequalities(data_hrg_count: DataFrame, fyears: list, min_count: int = 50) -> pd.DataFrame:
+
+def calculate_inequalities(
+    data_hrg_count: DataFrame, fyears: list, min_count: int = 50
+) -> pd.DataFrame:
     """Calculate inequalities
 
     :param data_hrg_count: The dataframe containing the counts of activity per IMD quintile and HRG for each provider and year
@@ -101,14 +108,14 @@ def calculate_inequalities(data_hrg_count: DataFrame, fyears: list, min_count: i
             )
             # remove HRGs where fewer than 3 quintiles are represented for that HRG
             hrg_filter = (
-                provider_df.groupby(["sushrg_trimmed"])[["imd19_quintile"]]
+                provider_df.groupby(["sushrg_trimmed"])[["imd_quintile"]]
                 .count()
                 .sort_index()
             )
-            hrg_filter = hrg_filter[hrg_filter["imd19_quintile"] > 2]
+            hrg_filter = hrg_filter[hrg_filter["imd_quintile"] > 2]
             hrgs_to_keep = list(hrg_filter.index)
             # remove HRGs where counts of hrg activity < min_count for an imd quintile
-            count_df = provider_df.groupby(["sushrg_trimmed", "imd19_quintile"])[
+            count_df = provider_df.groupby(["sushrg_trimmed", "imd_quintile"])[
                 ["count"]
             ].sum()
             hrgs_to_exclude = []
@@ -123,22 +130,25 @@ def calculate_inequalities(data_hrg_count: DataFrame, fyears: list, min_count: i
             for hrg in hrgs_to_include:
                 hrg_df = provider_df[provider_df["sushrg_trimmed"] == hrg]
                 y = hrg_df["activity_rate"]
-                x = sm.add_constant(hrg_df["imd19_quintile"])
+                x = sm.add_constant(hrg_df["imd_quintile"])
                 res = sm.WLS(y, x, weights=hrg_df["pop"]).fit()
                 results.append(
                     {
                         "provider": provider,
                         "fyear": fyear,
                         "sushrg_trimmed": hrg,
-                        "pvalue": res.pvalues.imd19_quintile,
-                        "slope": res.params.loc["imd19_quintile"],
+                        "pvalue": res.pvalues.imd_quintile,
+                        "slope": res.params.loc["imd_quintile"],
                         "intercept": res.params.loc["const"],
                     }
                 )
     linreg_df = pd.DataFrame(results)
     return linreg_df
 
-def process_calculated_inequalities(linreg_df: pd.DataFrame, data_hrg_count: DataFrame) -> pd.DataFrame:
+
+def process_calculated_inequalities(
+    linreg_df: pd.DataFrame, data_hrg_count: DataFrame
+) -> pd.DataFrame:
     """
     Process the calculated inequalities and return a dataframe containing the values for level_up, level_down, and zero_sum
 
@@ -160,28 +170,49 @@ def process_calculated_inequalities(linreg_df: pd.DataFrame, data_hrg_count: Dat
     )
     df = (
         data_hrg_count.toPandas()
-        .join(hrgs_with_inequalities, on=["provider", "sushrg_trimmed", "fyear"], how="inner")
-        .set_index(["provider", "sushrg_trimmed", "fyear", "imd19_quintile"])
+        .join(
+            hrgs_with_inequalities,
+            on=["provider", "sushrg_trimmed", "fyear"],
+            how="inner",
+        )
+        .set_index(["provider", "sushrg_trimmed", "fyear", "imd_quintile"])
         .sort_index()
-        .reset_index(level="imd19_quintile")
+        .reset_index(level="imd_quintile")
     )
     # Calculate values to use for level_up, level_down, and zero_sum for each combination of provider, fyear, and HRG
     for i in df.index:
-        mini_df = df.loc[i].set_index("imd19_quintile", append=True)
-        quintiles = mini_df.index.get_level_values("imd19_quintile")
-        mini_df.loc[:, "fitted_line"] = mini_df.loc[:, "intercept"] + mini_df.loc[:, "slope"] * quintiles
+        mini_df = df.loc[i].set_index("imd_quintile", append=True)
+        quintiles = mini_df.index.get_level_values("imd_quintile")
+        mini_df.loc[:, "fitted_line"] = (
+            mini_df.loc[:, "intercept"] + mini_df.loc[:, "slope"] * quintiles
+        )
         # Calculate level_up, set least deprived quintile factor to 1
-        mini_df.loc[:, "level_up"] = mini_df.loc[:, "fitted_line"].max() / mini_df.loc[:, "activity_rate"]
-        mini_df.loc[(slice(None), slice(None), slice(None), quintiles[-1]), "level_up"] = 1
+        mini_df.loc[:, "level_up"] = (
+            mini_df.loc[:, "fitted_line"].max() / mini_df.loc[:, "activity_rate"]
+        )
+        mini_df.loc[
+            (slice(None), slice(None), slice(None), quintiles[-1]), "level_up"
+        ] = 1
         # Calculate level_down, set most deprived quintile factor to 1
         if mini_df.loc[:, "fitted_line"].min() < 0:
             mini_df.loc[:, "level_down"] = 0
-        else: 
-            mini_df.loc[:, "level_down"] = mini_df.loc[:, "fitted_line"].min() / mini_df.loc[:, "activity_rate"]
-        mini_df.loc[(slice(None), slice(None), slice(None), quintiles[0]), "level_down"] = 1
+        else:
+            mini_df.loc[:, "level_down"] = (
+                mini_df.loc[:, "fitted_line"].min() / mini_df.loc[:, "activity_rate"]
+            )
+        mini_df.loc[
+            (slice(None), slice(None), slice(None), quintiles[0]), "level_down"
+        ] = 1
         # Calculate zero_sum
-        mini_df.loc[:, "zero_sum"] = mini_df.loc[:, "fitted_line"].mean() / mini_df.loc[:, "activity_rate"]
-        df.loc[i] = mini_df.reset_index("imd19_quintile")
+        mini_df.loc[:, "zero_sum"] = (
+            mini_df.loc[:, "fitted_line"].mean() / mini_df.loc[:, "activity_rate"]
+        )
+        # Add calculated values back to main dataframe
+        df.loc[i, ["fitted_line", "level_up", "level_down", "zero_sum"]] = (
+            mini_df.reset_index("imd_quintile")[
+                ["fitted_line", "level_up", "level_down", "zero_sum"]
+            ]
+        )
     return df
 
 
@@ -190,8 +221,7 @@ if __name__ == "__main__":
     fyears = [201920, 202223, 202324]
     spark = get_spark()
 
-    data_hrg_count = load_inequalities_data(fyears = fyears)
-    linreg_df = calculate_inequalities(data_hrg_count, fyears = fyears)
+    data_hrg_count = load_inequalities_data(fyears=fyears)
+    linreg_df = calculate_inequalities(data_hrg_count, fyears=fyears)
     inequalities = process_calculated_inequalities(linreg_df, data_hrg_count)
-
     inequalities.to_parquet(f"{path}/inequalities.parquet")
