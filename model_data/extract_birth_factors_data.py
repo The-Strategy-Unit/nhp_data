1322d9a58319d727663ef5a047abcb6ed3157ada
YiWen Hon <yiwen.hon1@nhs.net>
2025-06-05 12:31:06 +0100

    split extract_birth_factors, remove model_data_extract

--- model_data/extract_birth_factors_data.py
+++ model_data/extract_birth_factors_data.py
@@ -1,20 +1,14 @@
-"""Extract data for model"""
-
-# dbutils.widgets.text("version", "dev")
-# dbutils.widgets.text("fyear", "201920")
-
+"""Extract birth factors data for model"""
 
 import sys
-
 import pyspark.sql.functions as F
-from databricks.connect import DatabricksSession
 from pyspark.sql import DataFrame, SparkSession
-from model_data.helpers import create_population_projections
+from model_data.helpers import get_spark, create_population_projections
 
 
 # pylint: disable=invalid-name
 def create_custom_birth_factors_R0A66(
-    spark: SparkSession, birth_factors: DataFrame
+    birth_factors: DataFrame, spark: SparkSession = get_spark()
 ) -> DataFrame:
     """Create custom birth factors file for R0A66, using principal projection
 
@@ -30,7 +24,7 @@ def create_custom_birth_factors_R0A66(
 
 # pylint: disable=invalid-name
 def create_custom_birth_factors_RD8(
-    spark: SparkSession, birth_factors: DataFrame
+    birth_factors: DataFrame, spark: SparkSession = get_spark()
 ) -> DataFrame:
     """Create custom birth factors file for RD8, using principal projection
 
@@ -44,7 +38,9 @@ def create_custom_birth_factors_RD8(
     return custom_RD8
 
 
-def extract_birth_factors_data(spark: SparkSession, save_path: str, fyear: int) -> None:
+def extract_birth_factors_data(
+    save_path: str, fyear: int, spark: SparkSession = get_spark()
+) -> None:
     """Extract Birth Factors data
 
     :param spark: the spark context to use
@@ -74,30 +70,8 @@ def extract_birth_factors_data(spark: SparkSession, save_path: str, fyear: int) 
     )
 
 
-def main(save_path: str, fyear: int) -> None:
-    """Main method
-
-    :param save_path: where to save the parquet files
-    :type save_path: str
-    :param fyear: what year to extract
-    :type fyear: int
-    """
-
-    spark: SparkSession = DatabricksSession.builder.getOrCreate()
-
-    spark.catalog.setCurrentCatalog("nhp")
-    spark.catalog.setCurrentDatabase("default")
-
-    spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
-
-    extract_birth_factors_data(spark, save_path, fyear)
-
-
-def __init__(*args):
-    path = args[0]
-    fyear = int(args[1])
-    main(path, fyear)
-
-
 if __name__ == "__main__":
-    __init__(*sys.argv[1:])
+    path = sys.argv[1]
+    fyear = int(sys.argv[2])
+
+    extract_birth_factors_data(path, fyear)
