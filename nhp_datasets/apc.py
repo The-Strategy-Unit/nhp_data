efcfebfe1a2bd85b26912222288e80e95fe1620d
Tom Jemmett <thomas.jemmett@nhs.net>
2025-07-08 21:51:21 +0100

    moves the logic into the hes_apc object
    prevents 🐔and🥚 situation where provider_catchments needs nhp.raw_data.apc to be created in order to have mappings created

--- nhp_datasets/apc.py
+++ nhp_datasets/apc.py
@@ -3,12 +3,15 @@ from pyspark.sql import functions as F
 from pyspark.sql.types import *  # noqa: F403
 
 from nhp_datasets.icbs import icb_mapping
+from nhp_datasets.local_authorities import local_authority_successors
 from nhp_datasets.providers import read_data_with_provider
 
 spark = DatabricksSession.builder.getOrCreate()
 
 hes_apc = (
-    read_data_with_provider(spark, "hes.silver.apc")
+    local_authority_successors(
+        read_data_with_provider(spark, "hes.silver.apc"), "resladst_ons"
+    )
     .filter(F.col("last_episode_in_spell"))
     # remove well babies
     .filter(F.col("well_baby_ind") == "N")
