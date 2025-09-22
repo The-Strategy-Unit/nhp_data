The-Strategy-Unit/nhp_data

commit 912aa2290de2ec934fdad7c7ee219033985b6b2a

same, but for isNull -> isnull

src/nhp/data/nhp_datasets/apc.py
@@ -16,14 +16,14 @@ hes_apc = (
     .withColumn(
         "age",
         F.when(
-            (F.col("admiage") == 999) | F.col("admiage").isNull(),
+            (F.isnull("admiage") == 999) | F.col("admiage"),
             F.when(F.col("startage") > 7000, 0).otherwise(F.col("startage")),
         ).otherwise(F.col("admiage")),
     )
     .withColumn("age", F.when(F.col("age") > 90, 90).otherwise(F.col("age")))
     # remove well babies
     .filter(F.col("well_baby_ind") == "N")
-    .filter((F.col("sushrg") != "PB03Z") | F.col("sushrg").isNull())
+    .filter((F.isnull("sushrg") != "PB03Z") | F.col("sushrg"))
     .filter(~((F.col("tretspef") == "424") & (F.col("epitype") == "3")))
     # ---
     .filter(F.col("sex").isin(["1", "2"]))

src/nhp/data/raw_data/ecds.py
@@ -166,11 +166,11 @@ def get_ecds_data(spark: SparkSession) -> None:
             "is_discharged_no_treatment",
             (
                 (
-                    F.col("Der_EC_Investigation_All").isNull()
+                    F.isnull("Der_EC_Investigation_All")
                     | (F.col("Der_EC_Investigation_All") == "1088291000000101")
                 )
                 & (
-                    F.col("Der_EC_Treatment_All").isNull()
+                    F.isnull("Der_EC_Treatment_All")
                     | (F.col("Der_EC_Treatment_All") == "183964008")
                 )
             ),

src/nhp/data/raw_data/helpers.py
@@ -50,7 +50,7 @@ def add_tretspef_grouped_column(self: DataFrame) -> DataFrame:
 def add_age_group_column(self: DataFrame) -> DataFrame:
     return self.withColumn(
         "age_group",
-        F.when(F.col("age").isNull(), "Unknown")
+        F.when(F.isnull("age"), "Unknown")
         .when(F.col("age") == 0, "0")
         .when(F.col("age") <= 4, "1-4")
         .when(F.col("age") <= 9, "5-9")

src/nhp/data/raw_data/mitigators/ip/activity_avoidance/readmission_within_28_days.py
@@ -43,7 +43,7 @@ def _readmission_within_28_days():
         .filter(F.col("last_episode_in_spell"))
         # remove well babies
         .filter(F.col("well_baby_ind") == "N")
-        .filter((F.col("sushrg") != "PB03Z") | F.col("sushrg").isNull())
+        .filter((F.isnull("sushrg") != "PB03Z") | F.col("sushrg"))
         .filter(~((F.col("tretspef") == "424") & (F.col("epitype") == "3")))
         #
         .withColumnRenamed("person_id_deid", "person_id")

src/nhp/data/reference/provider_main_icb.py
@@ -11,8 +11,9 @@ def generate_provider_main_icb() -> None:
 
     w = Window.partitionBy("provider").orderBy(F.desc("count"))
 
+    F.isnotnull("icb")
     main_icbs = (
-        hes_apc.filter(F.col("icb").isNotNull())  # pylint: disable=undefined-variable
+        hes_apc.filter(F.isnotnull("icb"))
         .groupBy("provider", "icb")
         .count()
         .withColumn("row", F.row_number().over(w))

commit 6ecaf13bfa4eb8eb2433191c88320d0aa64d9836

fixes issue introduced in previous commit with null checks
912aa22 introduced some incorrect fixes with what looks like a bad search and replace

src/nhp/data/nhp_datasets/apc.py
@@ -16,14 +16,14 @@ hes_apc = (
     .withColumn(
         "age",
         F.when(
-            (F.isnull("admiage") == 999) | F.col("admiage"),
+            (F.col("admiage") == 999) | F.col("admiage").isNull(),
             F.when(F.col("startage") > 7000, 0).otherwise(F.col("startage")),
         ).otherwise(F.col("admiage")),
     )
     .withColumn("age", F.when(F.col("age") > 90, 90).otherwise(F.col("age")))
     # remove well babies
     .filter(F.col("well_baby_ind") == "N")
-    .filter((F.isnull("sushrg") != "PB03Z") | F.col("sushrg"))
+    .filter((F.col("sushrg") != "PB03Z") | F.col("sushrg").isNull())
     .filter(~((F.col("tretspef") == "424") & (F.col("epitype") == "3")))
     # ---
     .filter(F.col("sex").isin(["1", "2"]))