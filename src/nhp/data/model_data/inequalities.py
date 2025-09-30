b811ac8f172efeef1a0ad103734929e4f0726aeb
YiWen Hon <yiwen.hon1@nhs.net>
2025-09-30 14:00:10 +0100

    add missing providers to inequalities extract with fyear

--- src/nhp/data/model_data/inequalities.py
+++ src/nhp/data/model_data/inequalities.py
@@ -19,23 +19,42 @@ def extract(save_path: str, fyear: int, spark: SparkSession = get_spark()) -> No
     :type fyear: int
     """
 
-    providers = (
-        spark.read.table("nhp.default.apc")
-        .filter(F.col("fyear") == fyear)
-        .select("provider")
-        .distinct()
-    )
+    fyear_converted = fyear // 100
+
     inequalities = (
         spark.read.table("nhp.default.inequalities")
         .filter(F.col("fyear") == fyear)
-        .join(providers, on="provider", how="outer")
-        .withColumn("fyear", F.floor(F.col("fyear") / 100))
+        .withColumn("fyear", F.lit(fyear_converted))
         .withColumnRenamed("provider", "dataset")
     )
 
     (
         inequalities.repartition(1)
         .write.mode("overwrite")
+        .partitionBy(["fyear", "dataset"])
+        .parquet(f"{save_path}/inequalities")
+    )
+
+    # Write empty parquet files for missing providers
+    providers = (
+        spark.read.table("nhp.default.apc")
+        .filter(F.col("fyear") == fyear)
+        .select("provider")
+        .distinct()
+    )
+    missing_providers = (
+        providers.withColumnRenamed("provider", "dataset")
+        .join(inequalities.select("dataset").distinct(), on="dataset", how="left_anti")
+        .withColumn("fyear", F.lit(fyear_converted))
+    )
+    dummy_missing = missing_providers.join(
+        spark.createDataFrame([], inequalities.schema),
+        on=["fyear", "dataset"],
+        how="left",
+    )
+    (
+        dummy_missing.repartition(1)
+        .write.mode("append")
         .partitionBy(["fyear", "dataset"])
         .parquet(f"{save_path}/inequalities")
     )
