f35b8d3bda9e9377cd24af52fba7367689be34ac
Tom Jemmett <thomas.jemmett@nhs.net>
2026-04-28 09:18:22 +0100

    separate out sas token from the url, adds script to update the databricks secrets

--- src/nhp/data/extract_data.py
+++ src/nhp/data/extract_data.py
@@ -3,16 +3,7 @@
 You will need to manually generate a SAS token for both the data and the inputs
 data containers, and store these in databricks secrets.
 
-The SAS tokens should have create, write, and delete permissions, and should
-have as short an expiry time as possible (e.g. 1 day).
-
-Once you have generated the tokens, run
-
-    databricks secrets put-secret nhp MLCSU_DATA_SAS
-
-and
-
-    databricks secrets put-secret nhp MLCSU_INPUTS_DATA_SAS
+See the script update-sas-tokens.ps1.
 """
 
 import os
@@ -26,11 +17,15 @@ from tqdm.auto import tqdm
 from nhp.data.get_spark import get_spark
 
 
-def _move_files(path: str, connection_url: str, extract_version: str):
-    container = ContainerClient.from_container_url(connection_url)
+def _move_files(
+    path: str, account: str, container_name: str, sas: str, extract_version: str
+):
+    container = ContainerClient(
+        f"https://{account}.blob.core.windows.net/", container_name, credential=sas
+    )
     dlfs = DataLakeServiceClient(
-        connection_url.replace("blob", "dfs")
-    ).get_file_system_client(".")
+        f"https://{account}.dfs.core.windows.net/", credential=sas
+    ).get_file_system_client(container_name)
 
     # remove existing files, this may fail if the directory doesn't exist, but we can ignore that
     try:
@@ -40,7 +35,7 @@ def _move_files(path: str, connection_url: str, extract_version: str):
 
     # find files to upload
     all_files = list()
-    for root, dirs, files in os.walk(path):
+    for root, _dirs, files in os.walk(path):
         for file in files:
             src = os.path.join(root, file)
             dst = os.path.relpath(src, path)
@@ -55,13 +50,15 @@ def _move_files(path: str, connection_url: str, extract_version: str):
 
 
 def move_inputs_data(dbutils: DBUtils, path: str):
+    account = dbutils.secrets.get("nhp", "MLCSU_INPUTS_DATA_ACCOUNT")
     sas = dbutils.secrets.get("nhp", "MLCSU_INPUTS_DATA_SAS")
-    _move_files(f"{path}/inputs_data/dev", sas, "dev")
+    _move_files(f"{path}/inputs_data/dev", account, "inputs-data", sas, "dev")
 
 
 def move_data(dbutils: DBUtils, path: str):
+    account = dbutils.secrets.get("nhp", "MLCSU_DATA_ACCOUNT")
     sas = dbutils.secrets.get("nhp", "MLCSU_DATA_SAS")
-    _move_files(f"{path}/model_data/dev", sas, "dev")
+    _move_files(f"{path}/model_data/dev", account, "data", sas, "dev")
 
 
 def main():
