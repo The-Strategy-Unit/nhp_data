eb0af479a638a156cc7e9690d683ca57490b875c
Tom Jemmett <thomas.jemmett@nhs.net>
2026-09-08 19:06:22 +0100

    splits tpmas/functional areas into separate tasks

---
+++ src/nhp/data/model_data/ip_functional_areas.py
@@ -0,0 +1,52 @@
+"""Extract IP data for model"""
+
+import sys
+
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+
+from nhp.data.get_spark import get_spark
+from nhp.data.model_data.functional_areas.ip.beds import get_ip_functional_area_beds
+from nhp.data.model_data.functional_areas.ip.procedures import (
+    get_ip_functional_areas_procedures,
+)
+from nhp.data.table_names import table_names
+
+
+def extract(save_path: str, fyear: int, spark: SparkSession) -> None:
+    """Extract IP functional areas data
+
+    :param spark: the spark session to use
+    :type spark: SparkSession
+    :param save_path: where to save the parquet files
+    :type save_path: str
+    :param fyear: what year to extract
+    :type fyear: int
+    """
+    apc = spark.read.parquet(f"{save_path}/ip").filter(F.col("fyear") == fyear // 100)
+
+    (
+        get_ip_functional_area_beds(apc, spark)
+        .repartition(1)
+        .write.mode("overwrite")
+        .partitionBy(["fyear", "dataset"])
+        .parquet(f"{save_path}/ip_functional_areas_beds")
+    )
+
+    (
+        get_ip_functional_areas_procedures(apc, spark)
+        .repartition(1)
+        .write.mode("overwrite")
+        .partitionBy(["fyear", "dataset"])
+        .parquet(f"{save_path}/ip_functional_areas_procedures")
+    )
+
+
+def main():
+    data_version = sys.argv[1]
+    save_path = f"{table_names.model_data_path}/{data_version}"
+    fyear = int(sys.argv[2])
+
+    spark = get_spark()
+
+    extract(save_path, fyear, spark)
---
+++ src/nhp/data/model_data/ip_tpmas.py
@@ -0,0 +1,51 @@
+"""Extract IP data for model"""
+
+import sys
+
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+
+from nhp.data.get_spark import get_spark
+from nhp.data.table_names import table_names
+
+
+def extract(save_path: str, fyear: int, spark: SparkSession) -> None:
+    """Extract IP TPMAs data
+
+    :param spark: the spark session to use
+    :type spark: SparkSession
+    :param save_path: where to save the parquet files
+    :type save_path: str
+    :param fyear: what year to extract
+    :type fyear: int
+    """
+    apc = spark.read.parquet(f"{save_path}/ip").filter(F.col("fyear") == fyear // 100)
+
+    for k, v in [
+        ("activity_avoidance", "activity_avoidance"),
+        ("efficiencies", "efficiency"),
+    ]:
+        (
+            spark.read.table(table_names.default_apc_mitigators)
+            .filter(F.col("type") == v)
+            .filter(F.col("fyear") == fyear)
+            .drop("type", "fyear")
+            .withColumnRenamed("epikey", "rn")
+            .withColumnRenamed("provider", "dataset")
+            .join(apc, ["dataset", "rn"], "inner")
+            .select("dataset", "fyear", "rn", "strategy", "sample_rate")
+            .repartition(1)
+            .write.mode("overwrite")
+            .partitionBy(["fyear", "dataset"])
+            .parquet(f"{save_path}/ip_{k}_strategies")
+        )
+
+
+def main():
+    data_version = sys.argv[1]
+    save_path = f"{table_names.model_data_path}/{data_version}"
+    fyear = int(sys.argv[2])
+
+    spark = get_spark()
+
+    extract(save_path, fyear, spark)
