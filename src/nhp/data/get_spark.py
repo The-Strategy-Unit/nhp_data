caa0ca2f4f701db8e0badc1f80f83d77e0085fc5
Tom Jemmett <thomas.jemmett@nhs.net>
2026-09-23 09:12:54 +0100

    lazy load databricks module to allow code to be imported locally

--- src/nhp/data/get_spark.py
+++ src/nhp/data/get_spark.py
@@ -1,6 +1,5 @@
 """Helper method to get the spark session."""
 
-from databricks.connect import DatabricksSession
 from pyspark.sql import SparkSession
 
 
@@ -10,6 +9,10 @@ def get_spark() -> SparkSession:
     :return: get the spark session to use
     :rtype: SparkSession
     """
+    # if you load databricks.connect at module level, if you aren't running on
+    # databricks you can get errors
+    from databricks.connect import DatabricksSession
+
     spark = DatabricksSession.builder.getOrCreate()
     spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
     return spark
